f25a906c1cd43f14c6bc8483260ad770e5ba343b
Greg <greg@medrala.com>
2022-06-21 21:25:50 +0200

    test

--- file_ingest.py
+++ file_ingest.py
@@ -1,21 +1,22 @@
-def load_config():
+def load_config(file):
     import re
     #open file
-    with open('config.txt','r') as config:
+    with open(file,'r') as config:
         data = config.read()
 
     #regex out 2 capture groups, the name of the stanza and the remaining multiple lines of it's description
     stanza = re.findall('^([A-Za-z_]+)\s=\s(.+?)(?=^[A-Za-z_]+\s=|\Z)',data,re.DOTALL | re.MULTILINE)
 
-    global props_dict
     props_dict = {}
-    #populate dictionary with each pair of regex groups, the stanza name being the key
+    #populate dictionary with each pair of regex groups, the stanza name being the key. The previous objects was a tuple. I am moving the data to a dictionary because it allows me to search based on KEY.
     for row in stanza:
         props_dict[row[0]] = row[1]
 
-load_config()
-#check for value in dictionary
-if 'TRUNCATE' in props_dict:
-    print("found it!")
-else:
-    print("missing")
+    return props_dict
+
+# load_config('props.txt')
+# #check for value in dictionary
+# if 'TRUNCATE' in props_dict:
+#     print("found it!")
+# else:
+#     print("missing")
